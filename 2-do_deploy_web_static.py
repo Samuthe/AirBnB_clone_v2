36b1464f564dd6290c6bea055e0d5a623fa517c5
Samuthe <ekatisamuel@gmail.com>
2022-10-28 09:20:54 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,67 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
+"""
+The 2-do_deploy_web_static.pyy module
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy
+"""
+from os.path import isfile
+from fabric.api import put, run, env, local
+env.hosts = ['35.227.98.159', '104.196.65.233']
 
 
-from fabric.api import *
-from datetime import datetime
-from os.path import exists
+def do_pack():
+    """
+    generate a .tgz archive from web_static with do_pack
+    """
+    now = datetime.datetime.now()
+    archive = 'web_static_' + str(now.year) + str(now.month) +\
+        str(now.day) + str(now.hour) + str(now.minute) +\
+        str(now.second) + '.tgz'
 
+    if os.path.isdir("versions") is False:
+        local('mkdir -p versions')
 
-env.hosts = ['35.237.166.125', '54.167.61.201']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+    local("tar -cvzf versions/{} web_static".format(archive))
+    if (isfile('version/{}'.format(archive))):
+        return ('version/{}'.format(archive))
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
+    """distributes an archive to web servers using do_deploy func
     """
-    if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
-
-    try:
-        put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
-        return True
-    except ValueError:
+    if (isfile(archive_path)) is False:
         return False
+
+    noex = archive_path.split('/')[1].split('.')[0]
+    ex = archive_path.split('/')[1]
+    re = put(archive_path, "/tmp/")
+    if re.failed:
+        return False
+    re = run('mkdir -p /data/web_static/releases/{}/'.format(noex))
+    if re.failed:
+        return False
+    re = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+        ex, noex))
+    if re.failed:
+        return False
+    re = run('rm /tmp/{}.tgz'.format(noex))
+    if re.failed:
+        return False
+    re = run('mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}/'.format(noex, noex))
+    if re.failed:
+        return False
+    re = run('rm -rf /data/web_static/releases/{}/web_static'.format(noex))
+    if re.failed:
+        return False
+    re = run('rm -rf /data/web_static/current')
+    if re.failed:
+        return False
+    re = run('ln -s /data/web_static/releases/{}/\
+            /data/web_static/current'.format(noex))
+    if re.failed:
+        return False
+    print("New version deployed")
+    return True
